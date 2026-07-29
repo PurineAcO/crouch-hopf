182319b17b7666b796a70c7a5d74443fcc1b95d4
PurineRemote <purine@buaa.edu.cn>
2026-07-29 21:20:18 +0800

    四阶中心差分 debug

--- crouch/convect.py
+++ crouch/convect.py
@@ -10,13 +10,13 @@ def face_convect_mat_4rdmid(face:cc.face_class):
     if face.direction == 'S':
         G_list = [face.jacobi(cc.CellList[i][dj].F, cc.CellList[i][dj].G)[1] for dj in [j-2, j-1, j, j+1]]
         for pos, g, w in zip([13,11,7,3], G_list, [-1/12, 7/12, 7/12, -1/12]):
-            face.nei.form_influence(pos, -1 * w * g)
+            face.me.form_influence(pos, -1 * w * g)
     if face.direction == 'E':
         F_list = [face.jacobi(cc.CellList[di][j].F, cc.CellList[di][j].G)[0] for di in [i+2, i+1, i, i-1]]
-        for pos, g, w in zip([6,7,8,9], G_list, [-1/12, 7/12, 7/12, -1/12]):
-            face.nei.form_influence(pos, w * g)
+        for pos, g, w in zip([9,8,7,6], F_list, [-1/12, 7/12, 7/12, -1/12]):
+            face.me.form_influence(pos, w * g)
     if face.direction == 'W':
         F_list = [face.jacobi(cc.CellList[di][j].F, cc.CellList[di][j].G)[0] for di in [i-2, i-1, i, i+1]]
-        for pos, g, w in zip([5,6,7,8], G_list, [-1/12, 7/12, 7/12, -1/12]):
+        for pos, g, w in zip([5,6,7,8], F_list, [-1/12, 7/12, 7/12, -1/12]):
             face.me.form_influence(pos, -1 * w * g)
         
